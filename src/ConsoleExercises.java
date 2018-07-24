4e75ae6e9a6667431f64367c60bb6f218931a6cb
Christian Kenney <armyystrongg@Christians-MacBook-Air.local>
2018-07-24 14:11:09 -0500

    finished exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int userInt, length, width, total;
+        double userInt, length, width, total;
         String word1, word2, word3, sentence;
         System.out.println("Please enter a number");
-        userInt = sc.nextInt();
+        userInt = sc.nextDouble();
         System.out.println(userInt);
         System.out.println("Please enter 3 words");
         word1 = sc.next();
@@ -17,11 +17,13 @@ public class ConsoleExercises {
         sentence = sc.nextLine();
         System.out.println(sentence);
         System.out.println("What is the length of the class room?");
-        length = sc.nextInt();
+        length = sc.nextDouble();
         System.out.println("What is the width of the class room?");
-        width = sc.nextInt();
+        width = sc.nextDouble();
         total = (length * 2) + (width * 2);
         System.out.println("The total perimeter is: " + total);
+        double volume = length * width;
+        System.out.println("The total volume is: " + volume);
 
     }
 }
---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        long i = 1;
+        while(i < 100){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+                i++;
+            } else if(i % 3 == 0){
+                System.out.println("Fizz");
+                i++;
+            } else if(i % 5 == 0){
+                System.out.println("Buzz");
+                i++;
+            }else{
+                System.out.println(i);
+                i++;
+            }
+        }
+        System.out.print("What number do you want to go up to? ");
+        int userNum = sc.nextInt();
+        System.out.println("Here is your table! \n");
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+        for(var x = 1; x <= userNum; x++){
+            int squared = (x * x);
+            int cubed = (x * x * x);
+            System.out.println(x + "      | " + squared + "       | " + cubed);
+        }
+
+        boolean answer = true;
+        while(answer){
+            System.out.print("What is the grade? ");
+            int grade = sc.nextInt();
+            if(grade >= 90){
+                System.out.println("You recieved a A");
+            }else if(grade < 90 && grade >= 80 ){
+                System.out.println("You recieved a B");
+            }else if(grade < 80 && grade >= 70){
+                System.out.println("You recieved a C");
+            }else if(grade < 70 && grade >= 60){
+                System.out.println("You recieved a D");
+            }else{
+                System.out.println("You recieved a F");
+            }
+            System.out.println("Do you want to enter another grade? Y or N?");
+            String response = sc.next();
+            if(response.charAt(0) != 'Y'){
+                answer = false;
+            }
+        }
+
+    }
+}
