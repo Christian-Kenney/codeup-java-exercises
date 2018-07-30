9826fef848de3acf638e06ee6061bc6b7d2e1ef0
Christian Kenney <armyystrongg@Christians-MacBook-Air.local>
2018-07-30 10:10:22 -0500

    short object exercise

---
+++ src/RestaurantTest.java
@@ -0,0 +1,13 @@
+public class RestaurantTest {
+    public static void main(String[] args) {
+        RestaurantDish dish = new RestaurantDish();
+        dish.costInCents = 600;
+        dish.nameOfDish = "pasta";
+        dish.wouldRecommend = true;
+        System.out.println(dish.nameOfDish);
+        System.out.println(dish.costInCents);
+        System.out.println(dish.wouldRecommend);
+        dish.eat();
+    }
+
+}
